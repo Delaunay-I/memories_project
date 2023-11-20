4732952dbe2a8523f9961a5de1beeea64fe28af0
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2023-11-19 22:47:04 -0800

    Add new components and dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,37 @@
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Grow from '@mui/material/Grow';
+import Grid from '@mui/material/Grid';
+
+import Posts from './components/Posts/Post';
+import Form from './components/Form/Form';
+
+import memories from './images/memories.png';
+
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Container maxwidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height={60}/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
