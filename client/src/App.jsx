cbfc86e4d0e9424c03d0b2dd89f11e89f225a271
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2023-11-20 01:28:15 -0800

    Changed styling to tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,29 @@
-import Container from '@mui/material/Container';
-import AppBar from '@mui/material/AppBar';
-import Typography from '@mui/material/Typography';
-import Grow from '@mui/material/Grow';
-import Grid from '@mui/material/Grid';
+import Posts from "./components/Posts/Post";
+import Form from "./components/Form/Form";
+import memories from "./images/memories.png";
 
-import Posts from './components/Posts/Post';
-import Form from './components/Form/Form';
-
-import memories from './images/memories.png';
+import { styles } from "./styles";
 
 function App() {
-
   return (
-    <Container maxwidth='lg'>
-      <AppBar position='static' color='inherit'>
-        <Typography variant="h2" align="center">
-          Memories
-        </Typography>
-        <img src={memories} alt="memories" height={60}/>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
-  )
+    <div className="relative mx-auto w-[85%]">
+      <nav className="my-4 flex items-center justify-center py-6 rounded-lg shadow-xl h-20">
+        <h2 className="text-4xl bg-amber-600">Memories </h2>
+        <img src={memories} alt="memories" className="bg-amber-600 ml-4 h-16" />
+      </nav>
+
+      <section className="">
+        <div className="container mx-auto flex justify-between items-center py-4 bg-amber-200">
+          <div className="">
+            <Posts />
+          </div>
+          <div>
+            <Form />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
