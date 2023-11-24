5063c6619ae7ac30c25a7895313ed2dd2c14f127
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2023-11-24 11:55:09 -0800

    Refactor action types and dispatch calls

--- server/index.js
+++ server/index.js
@@ -1,25 +1,30 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv";
 
-const CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch((error) => console.log(error.message));
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
