a50fd37379fe49296b3e75b86fbc43ece9f16826
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2023-11-30 22:25:09 -0800

    Migrated createStore to configureStore

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import {
-  legacy_createStore as createStore,
-  applyMiddleware,
-  compose,
-} from "redux";
-import thunk from "redux-thunk";
 
-import reducers from "./reducers";
-
-import App from "./App.jsx";
 import "./index.css";
+import App from "./App.jsx";
+import { store } from "./store";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
